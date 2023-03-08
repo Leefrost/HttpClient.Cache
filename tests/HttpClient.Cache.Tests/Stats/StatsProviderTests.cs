288473e9a34c68f3041d745acba8f6e8b494d815
Leefrost <evertodante@gmail.com>
2023-03-08 16:14:26 +0200

    Added more tests for StatsProvider

--- tests/HttpClient.Cache.Tests/Stats/StatsProviderTests.cs
+++ tests/HttpClient.Cache.Tests/Stats/StatsProviderTests.cs
@@ -7,6 +7,17 @@ namespace HttpClient.Cache.Tests.Stats;
 public class StatsProviderTests
 {
     [Fact]
+    public void GetReport_GetDefaultEmptyReportIfNoActions_ReportSuccessful()
+    {
+        CacheStatsProvider provider = new("test-cache");
+        var expected = new StatsReport("test-cache");
+
+        var stats = provider.GetReport();
+
+        stats.Should().BeEquivalentTo(expected, ignore => ignore.Excluding(x => x.CreatedAt));
+    }
+    
+    [Fact]
     public void ReportHit_ReportCacheHitWith201_ReportSuccessful()
     {
         CacheStatsProvider provider = new("test-cache");
@@ -19,6 +30,24 @@ public class StatsProviderTests
         };
 
         provider.ReportHit(HttpStatusCode.Created);
+        var stats = provider.GetReport();
+
+        stats.Should().BeEquivalentTo(expected, ignore => ignore.Excluding(x => x.CreatedAt));
+    }
+    
+    [Fact]
+    public void ReportHit_ReportCacheMissWith503_ReportSuccessful()
+    {
+        CacheStatsProvider provider = new("test-cache");
+        var expected = new StatsReport("test-cache")
+        {
+            PerStatusCode = new Dictionary<HttpStatusCode, CacheStatsResult>
+            {
+                { HttpStatusCode.ServiceUnavailable, new CacheStatsResult { CacheHit = 0L, CacheMiss = 1L } }
+            }
+        };
+
+        provider.ReportMiss(HttpStatusCode.ServiceUnavailable);
         var stats = provider.GetReport();
 
         stats.Should().BeEquivalentTo(expected, ignore => ignore.Excluding(x => x.CreatedAt));
