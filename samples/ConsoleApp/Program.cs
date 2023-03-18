d5b368e3a5854ad84157f42e8e3eaba012dd5239
Leefrost <evertodante@gmail.com>
2023-03-18 21:46:15 +0200

    Bare samples

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Net;
+using HttpClient.Cache.InMemory;
+
+const string url = "http://worldclockapi.com/api/json/utc/now";
+
+var cacheExpiration = new Dictionary<HttpStatusCode, TimeSpan>()
+{
+    {HttpStatusCode.OK, TimeSpan.FromSeconds(60)},
+    {HttpStatusCode.BadRequest, TimeSpan.FromSeconds(10)},
+    {HttpStatusCode.InternalServerError, TimeSpan.FromSeconds(5)}
+};
+
+
+var httpClientHandler = new HttpClientHandler();
+var inMemoryResponseCache = new InMemoryCacheHandler(httpClientHandler, cacheExpiration);
+using (var client = new System.Net.Http.HttpClient(inMemoryResponseCache))
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write($"Try: {i}: {url}");
+        var stopwatch = Stopwatch.StartNew();
+
+        var result = client.GetAsync(url).GetAwaiter().GetResult();
+
+        var content = result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        Console.WriteLine($"Done in: {stopwatch.ElapsedMilliseconds} ms");
+        
+        Thread.Sleep(1000);
+    }
+}
+
+Console.WriteLine("Stats:");
+
+var stats = inMemoryResponseCache.StatsProvider.GetReport();
+Console.WriteLine($"Total: {stats.Total.TotalRequests}");
+Console.WriteLine($"-> Hit: {stats.Total.CacheHit} [{stats.Total.TotalHitsPercent}]");
+Console.WriteLine($"-> Miss: {stats.Total.CacheMiss} [{stats.Total.TotalMissPercent}]");
+Console.ReadLine();
