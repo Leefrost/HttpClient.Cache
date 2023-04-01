cea97485fd8ba2ea142fe4cf9ac235cf6bb2f5e1
Leefrost <evertodante@gmail.com>
2023-04-01 21:44:12 +0300

    Update docs

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -2,9 +2,9 @@
 using System.Net;
 using HttpClient.Cache.InMemory;
 
-const string url = "http://worldclockapi.com/api/json/utc/now";
+const string url = "https://randomuser.me/api/";
 
-//Set the cache time for each required status
+//Setting the cache time for each required status
 var cacheExpiration = new Dictionary<HttpStatusCode, TimeSpan>
 {
     {HttpStatusCode.OK, TimeSpan.FromSeconds(60)},
@@ -12,15 +12,14 @@ var cacheExpiration = new Dictionary<HttpStatusCode, TimeSpan>
     {HttpStatusCode.InternalServerError, TimeSpan.FromSeconds(5)}
 };
 
-//Client calls API and caches it
-//Report will show 1 Miss (initial) and 4 Hits. 
-var innerHandler = new HttpClientHandler();
-var cacheHandler = new InMemoryCacheHandler(innerHandler, cacheExpiration);
+//Calling the API and cache the responses
+var requestHandler = new HttpClientHandler();
+var cacheHandler = new InMemoryCacheHandler(requestHandler, cacheExpiration);
 using (var httpClient = new System.Net.Http.HttpClient(cacheHandler))
 {
     for (int i = 1; i <= 5; ++i)
     {
-        Console.Write($"Attempt: {i}: {url} ");
+        Console.Write($"Attempt {i}: {url}");
 
         var stopwatch = Stopwatch.StartNew();
         var result = await httpClient.GetAsync(url);
@@ -30,10 +29,12 @@ using (var httpClient = new System.Net.Http.HttpClient(cacheHandler))
         Console.WriteLine($"Done in: {stopwatch.ElapsedMilliseconds} ms");
         await Task.Delay(TimeSpan.FromSeconds(1));
     }
+    Console.WriteLine();
 }
 
+//Checking cache stats
 var stats = cacheHandler.StatsProvider.GetReport();
 Console.WriteLine($"Cache stats - total requests: {stats.Total.TotalRequests}");
-Console.WriteLine($"--> Hit: {stats.Total.CacheHit} [{stats.Total.TotalHitsPercent}]");
-Console.WriteLine($"--> Miss: {stats.Total.CacheMiss} [{stats.Total.TotalMissPercent}]");
+Console.WriteLine($"--> Hits: {stats.Total.CacheHit} [{stats.Total.TotalHitsPercent}]");
+Console.WriteLine($"--> Misses: {stats.Total.CacheMiss} [{stats.Total.TotalMissPercent}]");
 Console.ReadLine();
