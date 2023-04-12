1644ab3451ccfe7875aed7bb2f91dadd6523eb8c
Leefrost <evertodante@gmail.com>
2023-04-12 21:51:39 +0300

    Added couple descriptions

--- src/HttpClient.Cache/InMemory/Clock/ISystemClock.cs
+++ src/HttpClient.Cache/InMemory/Clock/ISystemClock.cs
@@ -1,6 +1,12 @@
 ﻿namespace HttpClient.Cache.InMemory.Clock;
 
+/// <summary>
+/// Defines cache clock
+/// </summary>
 public interface ISystemClock
 {
+    /// <summary>
+    /// Current time
+    /// </summary>
     DateTimeOffset UtcNow { get; }
 }
